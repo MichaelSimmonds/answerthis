fe85dbd1a6581f706523d364897d994c14b8178e
MichaelSimmonds <mrmichaelsimmonds@gmail.com>
2017-04-04 13:00:32 +0200

    add profile model

---
+++ db/migrate/20170404110002_create_profiles.rb
@@ -0,0 +1,17 @@
+class CreateProfiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :profiles do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :dob
+      t.string :gender
+      t.integer :is_client
+      t.string :company_name
+      t.string :comapny_contact
+      t.text :company_address
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404094758) do
+ActiveRecord::Schema.define(version: 20170404110002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.date     "dob"
+    t.string   "gender"
+    t.integer  "is_client"
+    t.string   "company_name"
+    t.string   "comapny_contact"
+    t.text     "company_address"
+    t.integer  "user_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -39,4 +54,5 @@ ActiveRecord::Schema.define(version: 20170404094758) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "profiles", "users"
 end
