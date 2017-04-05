908b5151fd9fae2c02df5953d2d0309c004005ea
MichaelSimmonds <mrmichaelsimmonds@gmail.com>
2017-04-05 17:45:16 +0200

    add questionnaire perms

--- app/admin/questionnaire.rb
+++ app/admin/questionnaire.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Questionnaire do
+
+  permit_params :title, :description, :profile_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
