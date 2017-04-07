076fe47b421c7e3d94013d9220ab8acf25117442
MichaelSimmonds <mrmichaelsimmonds@gmail.com>
2017-04-07 14:07:40 +0200

    select prize

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,17 +3,24 @@ class ResultsController < ApplicationController
   end
 
   def new
+    @result = Result.new
     @questionnaire = Questionnaire.find(params[:questionnaire_id])
-    @result = Result.new(questionnaire_id: @questionnaire.id, profile_id: current_profile.id)
     @prizes = Prize.where(questionnaire_id: @questionnaire.id)
     # redirect_to questionnaire_results_path(@questionnaire.id)
   end
 
   def create
-    # @result = Result.new(questionnaire_id: params[:questionnaire_id])
-    @result = Result.find(params[:result_id])
-    @result.prize_id = prize_params
+
+    @result = Result.new
+    @result.questionnaire_id = params[:questionnaire_id]
+    @result.prize_id = params[:result][:result]
+    @result.profile_id = current_profile.id
+    # @result = Result.find(params[:result_id])
     @result.save!
+    redirect_to root_path
+
+
+    # (questionnaire_id: @questionnaire.id, profile_id: current_profile.id)
     # redirect_to play_path(params[:questionnaire_id])
   end
 
@@ -33,6 +40,6 @@ class ResultsController < ApplicationController
   private
 
   def prize_params
-    params.require(:questionnaire).permit(:prize_id)
+    params.require(:questionnaire).permit(:prize_id, :result)
   end
 end
